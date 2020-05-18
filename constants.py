b4b1e61e7b038ab2586d2bf34e45d5c46d0676b4
pranjal-M8790 <58283553+pranjal-M8790@users.noreply.github.com>
2020-05-18 07:45:09 -0400

    Add files via upload

--- constants.py
+++ constants.py
@@ -1,32 +1,31 @@
-driver_path = 'chromedriver.exe'
-g_link = "https://accounts.google.com"
-identifier_element = "identifier"
-gmail_id = "pranjal@doctormm.com"
-xpath_next = '//*[@id="identifierNext"]/span/span'
-pass_element = "password"
-gmail_pass = "6P4M%X2[:jVsf9)f1352@7323451"
-xpath_login = '//*[@id="passwordNext"]/span/span'
-ss_url = "https://docs.google.com/spreadsheets/d/17pUrPnz7D-6pjqZkpG6VmlBhj2RBC3KsSP65dXON9hk/edit#gid=0"
+driverPath = 'chromedriver.exe'
+gLink = "https://accounts.google.com"
+identifierElement = "identifier"
+gmailId = "pranjal@doctormm.com"
+xpathNext = '//*[@id="identifierNext"]/span/span'
+passElement = "password"
+gmailPass = "6P4M%X2[:jVsf9)f1352@7323451"
+xpathLogin = '//*[@id="passwordNext"]/span/span'
+ssUrl = "https://docs.google.com/spreadsheets/d/17pUrPnz7D-6pjqZkpG6VmlBhj2RBC3KsSP65dXON9hk/edit#gid=0"
 
-ss_api = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-json_cred_path = 'survey_cred.json'
+ssApi = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+json = 'survey_cred.json'
 
-ss_name = 'NewSurvey_template_based1'
-s_name = 'Template1'
-data2insert = ["pain1", "progress", "Since your pain began HOW HAS IT CHANGED?", "", "radio", "yes", "improved, worsened, stayed the same"]
-insert_row = 4
+ssName = 'NewSurvey_template_based1'
+sName = 'Template1'
+data2Insert = ["pain1", "progress", "Since your pain began HOW HAS IT CHANGED?", "", "radio", "yes", "improved, worsened, stayed the same"]
+insertIndex = 4
 
-delete_row = 4
+deleteRow = 4
 
-update_row_number = 1
-update_col_number = 1
-update_value = "section"
+updateRowNumber = 1
+updateColNumber = 1
+updateValue = "section"
 
-
-xpath_ss_tools = '//*[@id="docs-tools-menu"]'
-xpath_ss_tools_script_editor = '//*[@id=":gq"]/div/span'
-xpath_gs_publish = '//*[@id="macros-publish-menu"]'
-xpath_gs_deploy = '//*[@id=":1m"]'
-xpath_gs_web_new = '//*[@value="New"]'
-xpath_gs_update = '//*[@class="gwt-Button action submit"]' 
-xpath_gs_latest_c = '//*[@id="dev-mode"]'
+xpathSsTools = '//*[@id="docs-tools-menu"]'
+xpathSsToolsScriptEditor = '//*[contains(@aria-label, "Script editor e")]'
+xpathGsPublish = '//*[@id="macros-publish-menu"]'
+xpathGsDeploy = '/html/body/div[13]/div[1]'
+xpathGsWebNew = '//*[@value="New"]'
+xpathGsUpdate = '//*[@class="gwt-Button action submit"]' 
+xpathGsLatestC = '//*[@id="dev-mode"]'
